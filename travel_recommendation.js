b4812cf283c70ba057da72242cff40cae139e765
Will <Willygomezr1@gmail.com>
2024-08-24 23:22:25 -0400

    working on the mobile menu

---
+++ travel_recommendation.js
@@ -0,0 +1,10 @@
+const menu = document.getElementById('menu-icon');
+const navBackground = document.querySelector('#nav-background')
+const nav = document.querySelector('nav a')
+
+menu.addEventListener('click', function() {
+    console.log("fuck you bitch ass");
+});
+
+
+
