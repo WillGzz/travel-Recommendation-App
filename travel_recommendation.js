99db83f4e553286e7642d9e1c09f4bbd048edbc0
Will <Willygomezr1@gmail.com>
2024-08-28 22:58:12 -0400

    mobile design

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,9 +1,29 @@
 const menu = document.getElementById('menu-icon');
-const navBackground = document.querySelector('#nav-background')
-const nav = document.querySelector('nav a')
+const closeIcon = document.getElementById('close-icon');
+const navBackground = document.getElementById('nav-background');
+const nav = document.getElementsByTagName('nav');
+const anchor = document.querySelectorAll('nav a');
+const imgBackground = document.getElementById('background-img');
+
+
+
 
 menu.addEventListener('click', function() {
-    console.log("fuck you bitch ass");
+    menu.style.display = 'none';
+    // nav.style.display = 'inline-block';
+    // anchor.forEach(anchor => {
+        //     anchor.style.display = 'block'; // Example: Make all anchor tags visible
+        // });
+    // nav.style.display = 'none';
+    closeIcon.style.display = 'block';
+    navBackground.classList.add('active');
+
+});
+
+closeIcon.addEventListener('click', function() {
+    closeIcon.style.display = 'none';
+    menu.style.display = 'block';
+    navBackground.classList.remove('active');
 });
 
 
